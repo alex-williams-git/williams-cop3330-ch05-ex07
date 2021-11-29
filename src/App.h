68410075e61b0d94ea48e06a795b012418dcf65e
Alexander Williams <alexander.williams@knights.ucf.edu>
2021-11-29 14:17:53 -0500

    added header file

---
+++ src/App.h
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Alexander Williams
+ */
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+double getPlusRoot(int a, int b, int c);
+
+double getMinusRoot(int a, int b, int c);
